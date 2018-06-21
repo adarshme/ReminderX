c909a5120b6d75bb008126b72d32d0e0f94940e5
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2018-06-21 15:43:14 +0530

    Added new reminder activity

---
+++ app/src/main/java/com/example/infernocoder/reminderx/NewReminder.java
@@ -0,0 +1,13 @@
+package com.example.infernocoder.reminderx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_reminder);
+    }
+}
